9f896895e9f59ff0ca828e595b6e32ca86fed48a
Your Name <controlsltrapidfire@gmail.comm>
2018-01-03 09:57:20 -0600

    styling some more

--- scriptsRapperGame2.js
+++ scriptsRapperGame2.js
@@ -6,29 +6,6 @@ audioElement.setAttribute('src', 'test123.mp3');
 audioElement.load()
 audioElement.getElementById = "playSnippet".addEventListener("load", function() {
   audioElement.play();
-//   $(".duration span").html(audioElement.duration);
-//   $(".filename span").html(audioElement.src);
-// },
-// true);
-//
-// audioElement.src;
-// audioElement.duration;
-//
-// audioElement.currentTime=35;ß
-// audioElement.play();
-
-// audio = $('test123.mp3', {
-// 'loop'      : 'loop',
-// 'autoplay'	: 'autoplay'
-// });
-//
-// if (audio.canPlayType('audio/mpeg')){
-// $('', {
-// 'src'	: 'test12.mp3',
-// 'type'	: 'audio/mpeg'
-// }).appendTo(audio);
-// }
-
 
 function Rapper (name, birthName, dateOfBirth, origin, song, lyric, picture) {
 
@@ -39,9 +16,9 @@ function Rapper (name, birthName, dateOfBirth, origin, song, lyric, picture) {
   this.song        = song;
   this.lyric       = lyric;
   this.picture     = picture;
-};
+}
 
-let rapper1 = new Rapper("Pimp C","Chad Butler","December 29,1973","Port Arthur, Texas","some song", "some lyric", "some picture")
+var rapper1 = new Rapper("Pimp C","Chad Butler","December 29,1973","Port Arthur, Texas","some song", "some lyric", "some picture");
 var rapper2 = new Rapper("Gucci Mane","Radric Davis","February 12, 1980","Atlanta, Georgia","some song", "some lyric", "some picture");
 var rapper3 = new Rapper("Rakim","William Griffin, Jr.","January 28, 1968","New York, New York","some song", "some lyric", "some picture");
 var rapper4 = new Rapper("Big Daddy Kane","Antonio Hardy","September 10, 1968", "New York, New York","some song", "some lyric", "some picture");
